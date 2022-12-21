9fda76accddb4c4f20e5ae74764ee4a80434d380
waqas ahmad <waqaskanju@gmail.com>
2022-12-21 23:09:40 +0500

    add rows to add_all_subjects_marks file

--- add_all_subjects_marks.php
+++ add_all_subjects_marks.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Add Marks of Students
+ * Add  All Subjects Marks of Students
  * php version 8.1
  *
- * @category Adfsad
+ * @category Exam
  *
- * @package Adf
+ * @package None
  *
- * @author Khan <abc@examp.com>
+ * @author Waqas <admin@waqaskanju.com>
  *
- * @license http://www.abc.com MIT
+ * @license http://www.waqaskanju.com/license MIT
  *
- * @link Adfas
+ * @link http://www.waqaskanju.com
  **/
 require_once 'db_connection.php';
 require_once 'sand_box.php';
@@ -99,23 +99,27 @@ if (isset($_GET['submit'])) {
   '$phy_marks'
 
   )";
-    $exe=mysqli_query($link, $q) or die('error'.mysqli_error($link));
+    $exe=mysqli_query($link, $q) 
+    or 
+    die('error in marks insertion'.mysqli_error($link));
     if ($exe) {
            echo
            "<div class='alert alert-success alert-dismissible'>
               <a href='#' class='close' data-dismiss='alert' aria-label='close'>
                  &times;</a>
-              <strong>Success!</strong> $roll_no   Added Successfully.
+              <strong>Success!</strong> $roll_no   Marks Added Successfully.
             </div>";
     } else {
-        echo 'error in submit';
+        echo 'error in insertion of marks';
     }
 }
-?>
-<?php
+
+/* This section is for add keyboard tab functionality to
+* minimize data entry time.
+*/
 /* Tab index block  */
 $query_index="SELECT * FROM tab_index";
-$execute_index=mysqli_query($link, $query_index) or die('error'.mysqli_error($link));
+$execute_index=mysqli_query($link, $query_index) or die('error in tab index selection'.mysqli_error($link));
 $index_result=mysqli_fetch_assoc($execute_index);
 $eng_index=$index_result['English'];
 $urd_index=$index_result['Urdu'];
@@ -137,126 +141,152 @@ $che_index=$index_result['Chemistry'];
 $phy_index=$index_result['Physics'];
 ?>
 
-<?php Page_header("Add Marks"); ?>
+<?php Page_header("Add All Subjects Marks"); ?>
 </head>
 <body>
   <div class="bg-warning text-center">
-    <h4>Enter Marks</h4>
+    <h4>Enter All Subjects Marks</h4>
   </div>
   <?php require_once 'nav.php';?>
 <div class="container">
-  <div class="row">
-    <div class="col-md-12">
       <form class="" action="#">
+      <div class="row">
         <div class="form-group">
-          <p class="p-2 text-primary font-weight-bold">
+          <small class="p-2 text-primary font-weight-bold">
             Note: Please Enter Roll No, Make sure this Roll No is
             already registered in students page,
-            other wise it will not work. For Absent Student add minus one as marks  (marks = -1)  </p>
+            other wise it will not work. For Absent Student add minus one as marks  (marks = -1)  </small>
           <label for="rollno">Roll No:</label>
           <span id="aj_result" class="text-danger"></span>
           <input type="number" class="form-control" id="rollno"  min="1"
           name="rollno" tabindex="1" placeholder="type roll no"
           autofocus required onfocusout="check_roll_no_marks()">
         </div>
-        <div class="form-row">
+</div> <!-- End of top row -->
+        <div class="row">
+          <!-- English Marks input -->
           <div class="form-group col-md-3">
           <label for="english">English:</label>
           <input type="number" class="form-control" id="english" max="100" min="-1"
                  name="eng" value="0" placeholder="type english marks"
-                 tabindex="<?php echo $eng_index ?>"  required>
+                 tabindex="<?php echo $eng_index ?>"  required />
           </div>
+          <!-- Urdu Marks input -->
           <div class="form-group col-md-3">
             <label for="urdu">Urdu:</label>
             <input type="number" class="form-control" id="urdu" max="100" min="-1"
             name="urd" value="0" placeholder="type urdu marks"
             tabindex="<?php echo $urd_index ?>" required>
           </div>
+          <!-- Maths Marks input -->
             <div class="form-group col-md-3">
               <label for="maths">Maths:</label>
               <input type="number" class="form-control" id="maths" max="100" min="-1"
               value="0" name="mat"  placeholder="type maths marks"
               tabindex="<?php echo $mat_index ?>" required>
             </div>
+            <!-- HPE Marks input -->
             <div class="form-group col-md-3">
               <label for="hpe">HPE:</label>
               <input type="number" class="form-control" id="hpe" max="100" min="-1"
               name="hpe" value="0" placeholder="type hpe marks"
               tabindex="<?php echo $hpe_index ?>" required>
             </div>
+</div> <!-- End of row 1 -->
+<div class="row">
+  <!-- Nazira Marks input -->
             <div class="form-group col-md-3">
               <label for="nazira">Nazira:</label>
               <input type="number" class="form-control" id="nazira" max="100" min="-1"
               name="naz" value="0" placeholder="type nazira marks"
               tabindex="<?php echo $naz_index ?>" required>
             </div>
+            <!-- Science Marks input -->
             <div class="form-group col-md-3">
               <label for="science">Science:</label>
               <input type="number" class="form-control" id="science" max="100" min="-1"
               name="sci" value="0" placeholder="type science marks"
               tabindex="<?php echo $sci_index ?>" required>
             </div>
+            <!-- Arabic Marks input -->
             <div class="form-group col-md-3">
               <label for="arabic">Arabic:</label>
               <input type="number" class="form-control" id="arabic" max="100" min="-1"
               name="ara" value="0" placeholder="type arabic marks"
               tabindex="<?php echo $ara_index ?>" required>
             </div>
+            <!-- Islamyat Marks input -->
             <div class="form-group col-md-3">
               <label for="islamyat">Islamyat:</label>
               <input type="number" class="form-control" id="islamyat" max="100"
               min="-1" name="isl" value="0" placeholder="type islamyat marks"
                tabindex="<?php echo $isl_index ?>" required>
             </div>
+</div>  <!-- End of row 2 -->
+<div class="row">
+      <!-- History And Geography Marks input -->  
             <div class="form-group col-md-3">
               <label for="history">History & Geopraphy:</label>
               <input type="number" class="form-control" id="history" max="100" min="-1"
                name="his" value="0" placeholder="type history and geography marks"
               tabindex="<?php echo $his_index ?>" required>
             </div>
+            <!-- Computer Science Marks input -->
             <div class="form-group col-md-3">
               <label for="cs">Computer Science:</label>
               <input type="number" class="form-control" id="cs" max="100" min="-1"
               name="csc" value="0" placeholder="type computer science marks"
               tabindex="<?php  echo$com_index ?> "required>
             </div>
+            <!-- Mutalia Quran Marks input -->
             <div class="form-group col-md-3">
               <label for="mutalia">Mutalia Quran:</label>
               <input type="number" class="form-control" id="mutalia" max="100" min="-1"
               name="mqu" value="0" placeholder="type mutalia Quran marks"
               tabindex="<?php echo $mut_index ?>" required>
             </div>
+            <!-- Qirat Marks input -->
             <div class="form-group col-md-3">
               <label for="qirat">Qirat:</label>
               <input type="number" class="form-control" id="qirat" max="100" min="-1"
               name="qir" value="0" placeholder="type qirat marks"
               tabindex="<?php echo $qir_index ?>" required>
             </div>
+</div> <!-- End of row 3 -->
+<div class="row">
+            <!-- Drawing Marks input -->
             <div class="form-group col-md-3">
               <label for="drawing">Drawing:</label>
               <input type="number" class="form-control" id="drawing" max="100" min="-1"
               name="dra" value="0" placeholder="type drawing marks"
               tabindex="<?php echo $dra_index ?>" required>
             </div>
+
+            <!-- Social Study / Pak Study Marks input -->
             <div class="form-group col-md-3">
-              <label for="social">Social Study:</label>
+              <label for="social">Social/Pak Study:</label>
               <input type="number" class="form-control" id="social" max="100" min="-1"
-              name="soc" value="0" placeholder="type social study marks"
+              name="soc" value="0" placeholder="type social/Pak study marks"
               tabindex="<?php echo $soc_index ?>" required>
             </div>
+            <!-- Pashto Marks input -->
             <div class="form-group col-md-3">
               <label for="pashto">Pashto:</label>
               <input type="number" class="form-control" id="pashto" max="100" min="-1"
               name="pas" value="0" placeholder="type pashto marks"
               tabindex="<?php echo $pas_index ?>" required>
             </div>
+
+            <!-- Biology Marks input -->
             <div class="form-group col-md-3">
               <label for="biology">Biology:</label>
               <input type="number" class="form-control" id="biology" max="100" min="-1"
               name="bio" value="0" placeholder="type biology marks"
               tabindex="<?php echo $bio_index ?>" required>
-            </div>
-
+             </div>
+</div> <!-- End of Row 4 -->
+<div class="row">
+  <!-- Chemistry Marks input -->
             <div class="form-group col-md-3">
               <label for="chemistry">Chemistry:</label>
               <input type="number" class="form-control" id="chemistry"
@@ -264,7 +294,7 @@ $phy_index=$index_result['Physics'];
               name="che" value="0" placeholder="type chemistry marks"
               tabindex="<?php echo $che_index ?>" required>
             </div>
-
+<!-- Physics Marks input -->
             <div class="form-group col-md-3">
               <label for="physics">Physics:</label>
               <input type="number" class="form-control" id="physics" max="100" min="-1"
@@ -272,11 +302,13 @@ $phy_index=$index_result['Physics'];
               tabindex="<?php echo $phy_index ?>" required>
             </div>
 
-        </div>
+            <div class=" col-md-3"><!-- Currently Empty 3rd div--> </div>
+
+<div class=" col-md-3"> <!-- Currently Empty 4th div--></div>
+</div> <!-- End of Row 4-->
           <button type="submit" name="submit" class="btn btn-primary">Submit</button>
       </form>
-    </div>
-  </div>
+    </div> 
   <div class="row">
     <div class="col-md-12">
       <table class="table table-bordered">
